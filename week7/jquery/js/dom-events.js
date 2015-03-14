a60a704c82b29c84bdf71d83a3ca5178a7ffc55c
Chris Jarvis <clj@ollure.com>
2015-03-14 10:45:04 -0500

    blues singers list

--- week7/jquery/js/dom-events.js
+++ week7/jquery/js/dom-events.js
@@ -2,4 +2,46 @@ var targetDiv = document.getElementById("mousey");
 var hiddenDiv = document.getElementById("hidden");
 
 
+// targetDiv.onmouseover = function () {
+// 	targetDiv.classList.remove('red');
+// 	targetDiv.classList.add('blue');
+// }
+
+// targetDiv.onmouseover = function () {
+// 	targetDiv.classList.toggle('red');
+// }
+
+// targetDiv.onmouseover = function () {
+// 	targetDiv.classList.remove('red');
+// }
+
+// targetDiv.onmouseout = function () {
+// 	targetDiv.classList.add('red');
+// }
+
+//why use JavaScript instead of CSS?
+
+// var peopleToDisplay = [];
+// peopleToDisplay.push("Robert Johnson");
+// peopleToDisplay.push("John Lee Hooker");
+// peopleToDisplay.push("T-Bone Walker");
+// peopleToDisplay.push("Muddy Waters");
+// peopleToDisplay.push("Big Mama Thornton");
+// peopleToDisplay.push("Robert Clay");
+
+// targetDiv.onmouseover = function () {
+// 	hiddenDiv.classList.toggle('hide');
+// 	hiddenDiv.innerHTML = peopleToDisplay.join("<br/>");
+// }
+
+// targetDiv.onmouseout = function () {
+// 	hiddenDiv.classList.add('hide');
+// }
+
+targetDiv.onclick = function () {
+ (alert("Hello World"));
+}
+
+
+
 
